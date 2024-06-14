457177e1a2cca1d9a7e489bfa7ed500dd30626ed
S.KOUSHIK <119282162+koushik7876@users.noreply.github.com>
2024-06-13 23:44:11 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// select side-navbar
+var sideNav = document.querySelector(".side-navbar")
+sideNav.style.display = "none"
+
+//Navbar show and close
+
+function showNavbar()
+{
+    sideNav.style.display="block"
+
+}
+
+function closeNavbar()
+{
+    sideNav.style.display="none"
+}
+
+
+
+
